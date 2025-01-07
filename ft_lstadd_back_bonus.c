4306bfb5a68dfd5f9a65d414556383b8899403ee
Simon Thomas <sithomas@z4r11p6.42lyon.fr>
2025-01-07 13:13:54 +0100

    change 1 more time

--- ft_lstadd_back_bonus.c
+++ ft_lstadd_back_bonus.c
@@ -6,7 +6,7 @@
 /*   By: sithomas <sithomas@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/13 17:18:19 by sithomas          #+#    #+#             */
-/*   Updated: 2025/01/07 13:09:06 by sithomas         ###   ########.fr       */
+/*   Updated: 2025/01/07 13:12:32 by sithomas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,9 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 
 	if (!new)
 		return ;
-	if (!lst || !(*lst))
+	if (!lst || !*lst)
 	{
-		lst = &new;
+		*lst = new;
 		return ;
 	}
 	last = ft_lstlast(*lst);
