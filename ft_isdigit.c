47f545492707f5ddd8bc2945c049b4899a64a332
Simon Thomas <simon.thomas.tux@gmail.com>
2025-01-28 14:18:19 +0100

    test

--- ft_isdigit.c
+++ ft_isdigit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_isdigit.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sithomas <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: sithomas <sithomas@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 13:51:11 by sithomas          #+#    #+#             */
-/*   Updated: 2024/11/06 13:45:26 by sithomas         ###   ########.fr       */
+/*   Updated: 2025/01/28 14:18:08 by sithomas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,6 @@
 int	ft_isdigit(int c)
 {
 	if (c >= '0' && c <= '9')
-		return (1);
+		return (3);
 	return (0);
 }
